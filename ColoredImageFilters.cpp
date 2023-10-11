7796eae7750f5127c236eb409f04882ab61544d7
lailawaleed <147208546+lailawaleed@users.noreply.github.com>
2023-10-11 22:55:57 +0300

    Update ColoredImageFilters.cpp

--- ColoredImageFilters.cpp
+++ ColoredImageFilters.cpp
@@ -207,6 +207,218 @@ void shrinkColoredImage(int shrinkScale){
     }
 }
 
+//============================================= Filter 1 ===============================================================
+void BlackAndWhite() {
+    for (int i = 0; i < SIZE; i++) {
+        for (int j = 0; j < SIZE; j++) {
+            int avg = 0;
+
+            // Calculate the average color intensity for the pixel (i, j,c)
+            for (int c = 0; c < RGB; c++) {
+                avg += image[i][j][c];
+            }
+            // Apply the black and white conversion
+            for (int c = 0; c < RGB; c++) {
+                if (avg / 3 > 127) {
+                    // Set the color channel to white (255)
+                    image[i][j][c] = WHITE;
+                } else {
+                    // Set the color channel to black (0)
+                    image[i][j][c] = BLACK;
+                }
+            }
+        }
+    }
+}
+
+//=========================================== Filter 4 =================================================================
+void FlipImage() {
+    char c;
+    cout << "Enter 'v' or 'h' as you want ^_^ :";
+    cin >> c;
+    if (c == 'h') {
+        HorizontalFlip();
+    } else if (c == 'v') {
+        VerticalFlip();
+    } else {
+        cout << "invalid choice\n";
+    }
+}
+void HorizontalFlip() {
+    for (int i = 0; i < SIZE; i++) {
+        for (int j = 0; j < SIZE / 2; j++) {
+            // Swap the pixels from the left half with their corresponding pixels in the right half
+            for (int c = 0; c < RGB; c++) {
+                swap(image[i][j][c], image[i][SIZE - 1 - j][c]);
+            }
+        }
+    }
+}
+void VerticalFlip() {
+    for (int i = 0; i < (SIZE / 2); ++i) {
+        for (int j = 0; j < SIZE; ++j) {
+            for (int k = 0; k < RGB; ++k) {
+                swap(image[i][j][k], image[SIZE - 1 - i][j][k]);
+            }
+        }
+    }
+}
+
+//=========================================== Filter 7 =================================================================
+
+void detectEdges()
+{
+    int sumR = 0, sumG = 0, sumB = 0;
+
+    // Calculate the sum of all pixel values in the image for each color channel
+    for (int i = 0; i < SIZE; ++i)
+    {
+        for (int j = 0; j < SIZE; ++j)
+        {
+            sumR += image[i][j][0]; // Red channel
+            sumG += image[i][j][1]; // Green channel
+            sumB += image[i][j][2]; // Blue channel
+        }
+    }
+    // Calculate the average pixel values for each channel
+    int avgR = (sumR / (SIZE * SIZE));
+    int avgG = (sumG / (SIZE * SIZE));
+    int avgB = (sumB / (SIZE * SIZE));
+
+    // Iterate over the image for edge detection
+    for (int i = 0; i < SIZE - 1; ++i) {
+        for (int j = 0; j < SIZE - 1; ++j) {
+            // Compare neighboring pixel values to the average for edge detection in each channel
+            if ((image[i][j][0] > avgR && image[i][j + 1][0] <= avgR) || (image[i][j][0] <= avgR && image[i][j + 1][0] > avgR) ||
+                (image[i][j][1] > avgG && image[i][j + 1][1] <= avgG) || (image[i][j][1] <= avgG && image[i][j + 1][1] > avgG) ||
+                (image[i][j][2] > avgB && image[i][j + 1][2] <= avgB) || (image[i][j][2] <= avgB && image[i][j + 1][2] > avgB))
+            {
+                image[i][j][0] = 0; // Set pixel to 0 in the Red channel if it's part of an edge
+                image[i][j][1] = 0; // Set pixel to 0 in the Green channel if it's part of an edge
+                image[i][j][2] = 0; // Set pixel to 0 in the Blue channel if it's part of an edge
+            }
+            else if ((image[i][j][0] <= avgR && image[i + 1][j][0] > avgR) || (image[i][j][0] > avgR && image[i + 1][j][0] <= avgR) ||
+                     (image[i][j][1] <= avgG && image[i + 1][j][1] > avgG) || (image[i][j][1] > avgG && image[i + 1][j][1] <= avgG) ||
+                     (image[i][j][2] <= avgB && image[i + 1][j][2] > avgB) || (image[i][j][2] > avgB && image[i + 1][j][2] <= avgB))
+            {
+                image[i][j][0] = 0; // Set pixel to 0 in the Red channel if it's part of an edge
+                image[i][j][1] = 0; // Set pixel to 0 in the Green channel if it's part of an edge
+                image[i][j][2] = 0; // Set pixel to 0 in the Blue channel if it's part of an edge
+            }
+            else
+            {
+                image[i][j][0] = 255; // Set pixel to 255 in the Red channel if it's not part of an edge
+                image[i][j][1] = 255; // Set pixel to 255 in the Green channel if it's not part of an edge
+                image[i][j][2] = 255; // Set pixel to 255 in the Blue channel if it's not part of an edge
+            }
+        }
+    }
+}
+
+//=========================================== Filter a =================================================================
+void MirrorImage() {
+    // get the user's choice
+    char choice;
+    cout << "Do you want left, right, upper, or lower Mirror side?\n";
+    cout << "Enter the first letter of your choice (e.g., 'l' for left) except lower enter 'd' : ";
+    cin >> choice;
+    // Apply the mirror filter based on user's choice
+    if (choice == 'l') {
+        // Mirror the left half of the image to the right
+        Left_Half_Mirror();
+    } else if (choice == 'r') {
+        // Mirror the right half of the image to the left
+        Right_Half_Mirror();
+    } else if (choice == 'u') {
+        // Mirror the upper half of the image to the lower half
+        Upper_Half_Mirror();
+    } else if (choice == 'd') {
+        // Mirror the lower half of the image to the upper half
+        Lower_Half_Mirror();
+    } else {
+        cout << "Invalid direction. Please enter 'l', 'r', 'u', or 'd'.\n";
+    }
+}
+void Left_Half_Mirror() {
+    // Iterate through the rows of the image
+    for (int x = 0; x < SIZE; x++) {
+        // Iterate through the columns in the left half of the image
+        for (int y = 0; y < SIZE / 2; y++) {
+            // Iterate through the color channels ( Red, Green, Blue)
+            for (int k = 0; k < RGB; ++k) {
+                // Set the pixel at (x, y, k) to be the same as the pixel on the opposite side of the mirror
+                image[x][y][k] = image[x][SIZE - 1 - y][k];
+            }
+        }
+    }
+}
+void Right_Half_Mirror() {
+    // Iterate through the rows of the image
+    for (int x = 0; x < SIZE; x++) {
+        // Iterate through the columns in the right half of the image
+        for (int y = SIZE / 2; y < SIZE; y++) {
+            // Iterate through the color channels (Red, Green, Blue)
+            for (int k = 0; k < RGB; ++k) {
+                // Set the pixel at (x, y, k) to be the same as the pixel on the opposite side of the mirror
+                image[x][y][k] = image[x][SIZE - 1 - y][k];
+            }
+        }
+    }
+}
+void Lower_Half_Mirror() {
+    // Iterate through the rows in the lower half of the image
+    for (int x = 0; x < SIZE / 2; x++) {
+        // Iterate through all the columns
+        for (int y = 0; y < SIZE; y++) {
+            // Iterate through the color channels (Red, Green, Blue)
+            for (int k = 0; k < RGB; k++) {
+                // Set the pixel at (x, y, k) to be the same as the pixel on the opposite side of the mirror
+                image[x][y][k] = image[SIZE - 1 - x][y][k];
+            }
+        }
+    }
+}
+void Upper_Half_Mirror() {
+    for (int x = SIZE / 2; x < SIZE; x++) {
+        // Iterate through all the rows in the top half of the image
+        for (int y = 0; y < SIZE; y++) {
+            // Iterate through the color channels (Red, Green, Blue)
+            for (int k = 0; k < RGB; ++k) {
+                // Set the pixel at (x, y, k) to be the same as the pixel on the opposite side of the mirror
+                image[x][y][k] = image[SIZE - 1 - x][y][k];
+            }
+        }
+    }
+}
+
+//=========================================== Filter d =================================================================
+void CropImage() {
+    int x, y, l, w;
+    // Get user input for the cropping parameters
+    cout << "Please enter the values of x, y, l, and w: ";
+    cin >> x >> y >> l >> w;
+
+    // Check if the cropping parameters are within bounds
+    if (x < 0 || x >= SIZE || y < 0 || y >= SIZE || l <= 0 || w <= 0 || x + l > SIZE || y + w > SIZE) {
+        cout << "Invalid cropping parameters. No crop applied.";
+    }
+
+    // Iterate through the entire image
+    for (int i = 0; i < SIZE; i++) {
+        for (int j = 0; j < SIZE; j++) {
+            for (int k = 0; k < RGB; ++k) 
+            {
+                if (i >= x && i < x + l && j >= y && j < y + w) {
+                    // Copy pixels inside the crop region as is
+                    image[i][j][k] = image[i][j][k];
+                } else {
+                    // Set pixels outside the crop region to black (color value 255)
+                    image[i][j][k] = 255;
+                }
+            }
+        }
+    }
+}
 
 // Function To save the colored Image After sppling Filters
 void saveColoredImage(char imageFileName[])
