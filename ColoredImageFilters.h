fbf93c539ee1fa8f64c13e1b6063dab8f288b633
lojayn2004 <111882914+lojayn2004@users.noreply.github.com>
2023-10-12 18:01:27 +0300

    Update ColoredImageFilters.h

--- ColoredImageFilters.h
+++ ColoredImageFilters.h
@@ -1,8 +1,19 @@
-//
-// Created by lojay on 10/12/2023.
-//
+#ifndef FILTER_COLOREDIMAGEFILTERS_H
+#define FILTER_COLOREDIMAGEFILTERS_H
 
-#ifndef IMAGE_FILTER_COLOREDIMAGEFILTERS_H
-#define IMAGE_FILTER_COLOREDIMAGEFILTERS_H
+void shrinkColoredImage(int shrinkScale);
 
+void changeColoredBrightness();
+
+void darkenColoredImage();
+
+void lightenColoredImage();
+
+void blurColoredImage();
+
+void skewColoredHorizontally(double  angle);
+
+void loadSecondColoredImage(char imageFileName[]);
+
+void saveColoredImage(char imageFileName[]);
 #endif //IMAGE_FILTER_COLOREDIMAGEFILTERS_H
